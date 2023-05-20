1d97359f4f05bdab1d45a86cffad63fc56078ff5
PollyTheParrrot <82878379+PollyTheParrrot@users.noreply.github.com>
2023-05-20 12:52:05 +0300

    TrackBall controll
    Testi controlloida cameraa onjectin ympärillä hiiren avulla

--- Threejs_TypeScript_InnoProject/src/client/client.ts
+++ Threejs_TypeScript_InnoProject/src/client/client.ts
@@ -1,22 +1,10 @@
+
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
-
-const light = new THREE.SpotLight()
-light.castShadow = true
-light.shadow.mapSize.width = 512
-light.shadow.mapSize.height = 512
-light.shadow.camera.near = 0.5
-light.shadow.camera.far = 100
-scene.add(light)
-
-//const helper = new THREE.SpotLightHelper(light)
-const helper = new THREE.CameraHelper(light.shadow.camera)
-scene.add(helper)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -24,80 +12,37 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
-camera.position.z = 7
+camera.position.z = 2
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-//renderer.shadowMap.type = THREE.BasicShadowMap
-//renderer.shadowMap.type = THREE.PCFShadowMap
-//renderer.shadowMap.type = THREE.VSMShadowMap
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+const controls = new TrackballControls(camera, renderer.domElement)
+// controls.addEventListener('change', () => console.log("Controls Change"))
+// controls.addEventListener('start', () => console.log("Controls Start Event"))
+// controls.addEventListener('end', () => console.log("Controls End Event"))
+// controls.enabled = false
+// controls.rotateSpeed = 1.0
+// controls.zoomSpeed = 1.2
+// controls.panSpeed = 0.8
+// controls.keys = ['KeyA', 'KeyS', 'KeyD']
+// controls.noPan = true //default false
+// controls.noRotate = true //default false
+// controls.noZoom = true //default false
+// controls.staticMoving = true //default false
+// controls.dynamicDampingFactor = 0.1
+// controls.maxDistance = 4
+// controls.minDistance = 2
 
-const planeGeometry = new THREE.PlaneGeometry(100, 20)
-const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial())
-plane.rotateX(-Math.PI / 2)
-plane.position.y = -1.75
-plane.receiveShadow = true
-scene.add(plane)
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true,
+})
 
-const torusGeometry = [
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-    new THREE.TorusGeometry(),
-]
-
-const material = [
-    new THREE.MeshBasicMaterial(),
-    new THREE.MeshLambertMaterial(),
-    new THREE.MeshPhongMaterial(),
-    new THREE.MeshPhysicalMaterial({}),
-    new THREE.MeshToonMaterial(),
-]
-
-const torus = [
-    new THREE.Mesh(torusGeometry[0], material[0]),
-    new THREE.Mesh(torusGeometry[1], material[1]),
-    new THREE.Mesh(torusGeometry[2], material[2]),
-    new THREE.Mesh(torusGeometry[3], material[3]),
-    new THREE.Mesh(torusGeometry[4], material[4]),
-]
-
-const texture = new THREE.TextureLoader().load('img/grid.png')
-material[0].map = texture
-material[1].map = texture
-material[2].map = texture
-material[3].map = texture
-material[4].map = texture
-
-torus[0].position.x = -8
-torus[1].position.x = -4
-torus[2].position.x = 0
-torus[3].position.x = 4
-torus[4].position.x = 8
-
-torus[0].castShadow = true
-torus[1].castShadow = true
-torus[2].castShadow = true
-torus[3].castShadow = true
-torus[4].castShadow = true
-
-torus[0].receiveShadow = true
-torus[1].receiveShadow = true
-torus[2].receiveShadow = true
-torus[3].receiveShadow = true
-torus[4].receiveShadow = true
-
-scene.add(torus[0])
-scene.add(torus[1])
-scene.add(torus[2])
-scene.add(torus[3])
-scene.add(torus[4])
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -110,68 +55,11 @@ function onWindowResize() {
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const data = {
-    color: light.color.getHex(),
-    mapsEnabled: true,
-    shadowMapSizeWidth: 512,
-    shadowMapSizeHeight: 512,
-}
-
-const gui = new GUI()
-const lightFolder = gui.addFolder('THREE.Light')
-lightFolder.addColor(data, 'color').onChange(() => {
-    light.color.setHex(Number(data.color.toString().replace('#', '0x')))
-})
-lightFolder.add(light, 'intensity', 0, 1, 0.01)
-
-const spotLightFolder = gui.addFolder('THREE.SpotLight')
-spotLightFolder.add(light, 'distance', 0, 100, 0.01)
-spotLightFolder.add(light, 'decay', 0, 4, 0.1)
-spotLightFolder.add(light, 'angle', 0, 1, 0.1)
-spotLightFolder.add(light, 'penumbra', 0, 1, 0.1)
-spotLightFolder
-    .add(light.shadow.camera, 'near', 0.1, 100)
-    .onChange(() => light.shadow.camera.updateProjectionMatrix())
-spotLightFolder
-    .add(light.shadow.camera, 'far', 0.1, 100)
-    .onChange(() => light.shadow.camera.updateProjectionMatrix())
-spotLightFolder
-    .add(data, 'shadowMapSizeWidth', [256, 512, 1024, 2048, 4096])
-    .onChange(() => updateShadowMapSize())
-spotLightFolder
-    .add(data, 'shadowMapSizeHeight', [256, 512, 1024, 2048, 4096])
-    .onChange(() => updateShadowMapSize())
-spotLightFolder.add(light.position, 'x', -50, 50, 0.01)
-spotLightFolder.add(light.position, 'y', -50, 50, 0.01)
-spotLightFolder.add(light.position, 'z', -50, 50, 0.01)
-spotLightFolder.open()
-
-function updateShadowMapSize() {
-    light.shadow.mapSize.width = data.shadowMapSizeWidth
-    light.shadow.mapSize.height = data.shadowMapSizeHeight
-    ;(light.shadow.map as any) = null
-}
-
-const meshesFolder = gui.addFolder('Meshes')
-meshesFolder.add(data, 'mapsEnabled').onChange(() => {
-    material.forEach((m) => {
-        if (data.mapsEnabled) {
-            m.map = texture
-        } else {
-            m.map = null
-        }
-        m.needsUpdate = true
-    })
-})
-
 function animate() {
     requestAnimationFrame(animate)
 
-    helper.update()
-
-    torus.forEach((t) => {
-        t.rotation.y += 0.01
-    })
+    // trackball controls needs to be updated in the animation loop before it will work
+    controls.update()
 
     render()
 
